683ba926506f65e844f6c0514bef9242b15f35b3
Amos <amoswhitewolf@gmail.com>
2016-10-18 10:58:19 +1100

    Update madeline styling

--- public/js/dev/common.js
+++ public/js/dev/common.js
@@ -1,4 +1,20 @@
 (function ($) {
+
+    $(document).ready(function() {
+        $('.show-comments').on('click', function(){
+            var disqus_shortname = 'cognitives-io'; // Replace this value with *your* username.
+
+            // ajax request to load the disqus javascript
+            $.ajax({
+                type: "GET",
+                url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+                dataType: "script",
+                cache: true
+            });
+            // hide the button once comments load
+            $(this).fadeOut();
+        });
+    });
     
       $(window).resize(function(){
         if ($('.side-navigation').is(':visible')) {
